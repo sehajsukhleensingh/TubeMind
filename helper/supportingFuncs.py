a71452dcfb1fffd3ebf566ae85e0573384962860
sehaj <singhsehajsukhleen@gmail.com>
2026-01-07 01:21:24 +0530

    moved the supporting Funcs to a diff helper file

---
+++ helper/supportingFuncs.py
@@ -0,0 +1,66 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import re
+
+
+# extracts the ytVid ID from its url 
+def ytUrlId(url: str) -> str:
+    """
+    this function takes Youtube video url as the input and returns the id from the url 
+    INPUT:
+        url
+    OUPUT:
+        url id
+    """
+    try:
+        # using the regex 
+        pattern = r"(?:v=|\/)([0-9A-Za-z_-]{11}).*"
+        match = re.search(pattern, url)
+
+        return match.group(1) if match else None
+    
+    except:
+        return f"Invalid url {url}"
+
+
+# uses the ytTranscript Api to fetch the transcripts of a yt video 
+def transcript(ytID: str , lang: str = "en") -> str:
+    """
+    this function takes Youtube video id as the input and returns the transcripts
+    of the video in the language entered by user , default english (en)
+    INPUT:
+        yt video id 
+    OUPUT:
+        transcript of the video
+    """
+    ytAPI = YouTubeTranscriptApi()
+
+    try:
+        res = ytAPI.fetch(ytID , languages = [lang])
+        # fetches the each text part from different time snippets
+        transcript = [text.text for text in res.snippets]
+        # combines the whole different "strs" list into a single big "str" 
+        transcript = "".join(transcript)
+        return transcript
+    
+    except Exception as e:
+        return f"failed to fetch the transcript , error = {e}"
+
+    
+def createChunks(transcript: str) -> str:
+    """
+    this function takes the transcript as the input and create the chuncks of it 
+    """
+    splitter = RecursiveCharacterTextSplitter(chunk_size = 1000 , chunk_overlap = 100)
+    doc = splitter.create_documents([transcript])
+ 
+    return doc
+
+def fetchPrompt(path:str) -> str:
+    """
+    this function fetches the textual prompts for diff purposes 
+
+    """
+
+    with open(path , "r" , encoding= "utf-8") as file:
+        return file.read()
