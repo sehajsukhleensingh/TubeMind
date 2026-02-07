57cfa1c808792075cd33c2573f56fee9b82d53d1
Sehaj Sukhleen Singh <111988584+sehajsukhleensingh@users.noreply.github.com>
2026-02-07 22:50:08 +0530

    Update model version in ChatGoogleGenerativeAI

--- api/app.py
+++ api/app.py
@@ -31,7 +31,7 @@ if not hfkey:
 
 
 llm = ChatGoogleGenerativeAI(
-    model = "gemini-2.5-flash-lite",
+    model = "gemini-3.0-flash-lite",
     temperature = 0.2 , 
     google_api_key = googlekey
 )
